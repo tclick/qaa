016eccb92875609c419a823d7bd662a64a571bb5
Timothy Click <tclick@okstate.edu>
2021-03-19 20:41:41 -0500

    test: add definition of data files

---
+++ tests/datafile.py
@@ -0,0 +1,26 @@
+# --------------------------------------------------------------------------------------
+#  Copyright (C) 2020–2021 by Timothy H. Click <tclick@okstate.edu>
+#
+#  Permission to use, copy, modify, and/or distribute this software for any purpose
+#  with or without fee is hereby granted.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+#  REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+#  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+#  INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+#  OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+#  TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+#  THIS SOFTWARE.
+# --------------------------------------------------------------------------------------
+"""Various data files for testing."""
+
+from pathlib import Path
+
+from pkg_resources import resource_filename
+
+__all__ = ["TOPWW", "TRJWW"]
+
+TOPWW = resource_filename(
+    __name__, Path().joinpath("data", "rnase2_nowat.parm7").as_posix()
+)
+TRJWW = resource_filename(__name__, Path().joinpath("data", "protein.nc").as_posix())
