16c7642f758e08e02c78a7ec07c048d3c978cab2
Timothy Click <tclick@okstate.edu>
2021-03-19 20:27:07 -0500

    feat: add utility function to get coordinates

--- src/qaa/libs/utils.py
+++ src/qaa/libs/utils.py
@@ -12,3 +12,39 @@
 #  TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 #  THIS SOFTWARE.
 # --------------------------------------------------------------------------------------
+"""Various utilities."""
+import MDAnalysis as mda
+
+from .typing import ArrayLike, PathLike, Universe
+
+
+def get_coordinates(topology: PathLike, trajectory: PathLike) -> ArrayLike:
+    """Read a molecular dynamics trajectory and retrieve the coordinates.
+
+    Parameters
+    ----------
+    topology : PathLike
+        Topology file
+    trajectory : PathLike
+        Trajectory file
+
+    Returns
+    -------
+    Array
+        The coordinates with shape (n_frames, n_atoms, 3)
+    """
+    universe: Universe = mda.Universe(topology, trajectory)
+    n_atoms = universe.atoms.n_atoms
+    n_frames = universe.trajectory.n_frames
+    pos: ArrayLike
+
+    if n_atoms * n_frames >= 10_000_000:
+        import dask.array as da
+
+        pos = da.from_array([universe.atoms.positions for _ in universe.trajectory])
+    else:
+        import numpy as np
+
+        pos = np.asarray([universe.atoms.positions for _ in universe.trajectory])
+
+    return pos
