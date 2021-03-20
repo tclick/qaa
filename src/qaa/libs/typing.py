dd2eecee576dd716a81fc903e8bb48d82b99a0ce
Timothy Click <tclick@okstate.edu>
2021-03-19 22:15:03 -0500

    chore: update copyright year

--- src/qaa/libs/typing.py
+++ src/qaa/libs/typing.py
@@ -1,5 +1,5 @@
 # --------------------------------------------------------------------------------------
-#  Copyright (C) 2021 by Timothy H. Click <tclick@okstate.edu>
+#  Copyright (C) 2020–2021 by Timothy H. Click <tclick@okstate.edu>
 #
 #  Permission to use, copy, modify, and/or distribute this software for any purpose
 #  with or without fee is hereby granted.
