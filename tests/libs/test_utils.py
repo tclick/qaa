55693040dd2e51a6373b63f715266617bfb0122b
Timothy Click <tclick@okstate.edu>
2021-03-22 22:36:33 -0500

    test: test both array and xarray versions

--- tests/libs/test_utils.py
+++ tests/libs/test_utils.py
@@ -15,6 +15,7 @@
 import MDAnalysis as mda
 import numpy as np
 import pytest
+import xarray as xr
 from numpy import testing
 
 from qaa.libs import utils
@@ -35,24 +36,62 @@ class TestUtils:
     def n_frames(self, universe: mda.Universe) -> int:
         return universe.trajectory.n_frames
 
-    def test_positions(self, universe: mda.Universe, n_atoms: int, n_frames: int):
+    def test_positions_xarray(
+        self, universe: mda.Universe, n_atoms: int, n_frames: int
+    ):
         """
         GIVEN topology and trajectory filenames
         WHEN the get_positions function is called
-        THEN return an array of positions with shape (n_frames, n_atoms, 3)
+        THEN return an xarray of positions with shape (n_frames, n_atoms, 3)
         """
         array = utils.get_positions(TOPWW, TRJWW)
         assert array.shape == (n_frames, n_atoms, 3)
         testing.assert_allclose(array[0], universe.atoms.positions)
+        assert isinstance(array, xr.DataArray)
 
         universe.trajectory[-1]
         testing.assert_allclose(array[-1], universe.atoms.positions)
 
-    def test_reshape(self, universe: mda.Universe, n_atoms: int, n_frames: int):
+    def test_positions_array(self, universe: mda.Universe, n_atoms: int, n_frames: int):
+        """
+        GIVEN topology and trajectory filenames
+        WHEN the get_positions function is called
+        THEN return a array of positions with shape (n_frames, n_atoms, 3)
+        """
+        array = utils.get_positions(TOPWW, TRJWW, return_type="array")
+        assert array.shape == (n_frames, n_atoms, 3)
+        testing.assert_allclose(array[0], universe.atoms.positions)
+        assert isinstance(array, np.ndarray)
+
+        universe.trajectory[-1]
+        testing.assert_allclose(array[-1], universe.atoms.positions)
+
+    def test_reshape_array(self, universe: mda.Universe, n_atoms: int, n_frames: int):
+        """
+        GIVEN a coordinate matrix of shape (n_frames, n_points, 3)
+        WHEN calling the reshape_position function
+        THEN a 2D-array of shape (n_frames, n_points * 3) will be returned
+        """
         positions = np.array([universe.atoms.positions for _ in universe.trajectory])
         new_positions = utils.reshape_positions(positions)
 
         assert new_positions.shape == (n_frames, n_atoms * 3)
+        assert isinstance(new_positions, np.ndarray)
+
+    def test_reshape_xarray(self, universe: mda.Universe, n_atoms: int, n_frames: int):
+        """
+        GIVEN a coordinate matrix of shape (n_frames, n_points, 3)
+        WHEN calling the reshape_position function
+        THEN a 2D-array of shape (n_frames, n_points * 3) will be returned
+        """
+        positions = xr.DataArray(
+            [universe.atoms.positions for _ in universe.trajectory],
+            dims="frame name dim".split(),
+        )
+        new_positions = utils.reshape_positions(positions)
+
+        assert new_positions.shape == (n_frames, n_atoms * 3)
+        assert isinstance(new_positions, xr.DataArray)
 
     def test_rmse(self, universe):
         """
@@ -62,4 +101,4 @@ class TestUtils:
         """
         positions = universe.select_atoms("name CA").positions
         error = utils.rmse(positions, positions)
-        testing.assert_allclose(error, 0.)
+        testing.assert_allclose(error, 0.0)
