5cec54b58cd21c52f7bde0eb3331c110714beb87
Timothy Click <tclick@okstate.edu>
2021-04-08 10:43:14 -0500

    docs: include sphinx-automodapi

--- noxfile.py
+++ noxfile.py
@@ -211,7 +211,12 @@ def docs_build(session: Session) -> None:
     """Build the documentation."""
     args = session.posargs or ["docs", "docs/_build"]
     session.install(".")
-    session.install("sphinx", "sphinx-click", "sphinx-rtd-theme")
+    session.install(
+        "sphinx",
+        "sphinx-click",
+        "sphinx-rtd-theme",
+        "sphinx_automodapi",
+    )
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
@@ -225,7 +230,13 @@ def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
     session.install(".")
-    session.install("sphinx", "sphinx-autobuild", "sphinx-click", "sphinx-rtd-theme")
+    session.install(
+        "sphinx",
+        "sphinx-autobuild",
+        "sphinx-click",
+        "sphinx-rtd-theme",
+        "sphinx_automodapi",
+    )
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
