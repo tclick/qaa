b4b0707a38398599a182ead1689bec1b257c6575
Timothy Click <tclick@alumni.ou.edu>
2021-04-16 16:37:03 -0500

    feat: save plots
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

--- src/qaa/commands/cmd_plot.py
+++ src/qaa/commands/cmd_plot.py
@@ -13,3 +13,179 @@
 #  THIS SOFTWARE.
 # --------------------------------------------------------------------------------------
 """Subcommand to plot data."""
+import logging.config
+import time
+from pathlib import Path
+from typing import Optional
+from typing import Tuple
+
+import click
+import numpy as np
+import pandas as pd
+
+from .. import create_logging_dict
+from .. import PathLike
+from ..libs.figure import Figure
+
+
+@click.command("cluster", short_help="Plot data from QAA.")
+@click.option(
+    "-i",
+    "--infile",
+    metavar="FILE",
+    default=Path.cwd().joinpath("input.csv"),
+    show_default=True,
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True),
+    help="Data file for analysis",
+)
+@click.option(
+    "--label",
+    metavar="FILE",
+    default=Path.cwd().joinpath("labels.npy"),
+    show_default=True,
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, resolve_path=True),
+    help="Cluster labels",
+)
+@click.option(
+    "-c",
+    "--centroid",
+    metavar="FILE",
+    default=Path.cwd().joinpath("centroids.csv"),
+    show_default=True,
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, resolve_path=True),
+    help="Cluster labels",
+)
+@click.option(
+    "-o",
+    "--outfile",
+    metavar="FILE",
+    default=Path.cwd().joinpath("cluster.png"),
+    show_default=True,
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, resolve_path=True),
+    help="Image file",
+)
+@click.option(
+    "-l",
+    "--logfile",
+    metavar="LOG",
+    show_default=True,
+    default=Path.cwd() / "plot.log",
+    type=click.Path(exists=False, file_okay=True, resolve_path=True),
+    help="Log file",
+)
+@click.option(
+    "--axes", nargs=3, default=(0, 1, 2), type=click.IntRange(min=0, clamp=True)
+)
+@click.option("--ica / --pca", "method", default=True, help="Type of data")
+@click.option(
+    "--dpi",
+    default=600,
+    show_default=True,
+    type=click.IntRange(min=100, clamp=True),
+    help="Resolution of the figure",
+)
+@click.option(
+    "--azim",
+    "azimuth",
+    default=120,
+    show_default=True,
+    type=click.IntRange(min=0, max=359, clamp=True),
+    help="Azimuth rotation of 3D plot",
+)
+@click.option(
+    "--elev",
+    "elevation",
+    default=30,
+    show_default=True,
+    type=click.IntRange(min=0, max=90, clamp=True),
+    help="Elevation of 3D plot",
+)
+@click.option("-v", "--verbose", is_flag=True, help="Noisy output")
+def cli(
+    infile: PathLike,
+    label: PathLike,
+    centroid: PathLike,
+    outfile: PathLike,
+    logfile: PathLike,
+    axes: Tuple[int, int, int],
+    method: bool,
+    dpi: int,
+    azimuth: int,
+    elevation: int,
+    verbose: bool,
+) -> None:
+    """Visualize the data."""
+    start_time: float = time.perf_counter()
+    in_file = Path(infile)
+
+    # Setup logging
+    logging.config.dictConfig(create_logging_dict(logfile))
+    logger: logging.Logger = logging.getLogger(__name__)
+
+    data_method = "ica" if method else "pca"
+    sorted_axes = np.sort(axes)
+    features = [f"{data_method[:2].upper()}{_+1:d}" for _ in sorted_axes]
+
+    # Load data
+    logger.info("Loading %s", in_file)
+
+    data = read_file(in_file, index="Frame")
+    if data is None:
+        raise SystemExit(f"Could not open {in_file}")
+    else:
+        data.columns = [
+            f"{data_method[:2].upper()}{_+1:d}" for _ in range(data.columns.size)
+        ]
+        data = data[features]
+
+    # Load labels, if exists
+    label_data = read_file(Path(label), index="Frame")
+    if "Cluster" not in data.columns and label_data is not None:
+        label_data.columns = ["Cluster"]
+        data = pd.concat([label_data, data], axis=1)
+
+    # Load centroid data, if exists
+    centroid_data = read_file(Path(centroid), index="Cluster")
+    if centroid_data is not None:
+        centroid_data.columns = features
+
+    # Prepare cluster analysis
+    figure = Figure(azim=azimuth, elevation=elevation)
+    figure.draw(data, centers=centroid_data)
+
+    logger.info("Saving cluster data to %s", outfile)
+    figure.save(outfile, dpi=dpi)
+
+    stop_time: float = time.perf_counter()
+    dt: float = stop_time - start_time
+    struct_time: time.struct_time = time.gmtime(dt)
+    if verbose:
+        output: str = time.strftime("%H:%M:%S", struct_time)
+        logger.info(f"Total execution time: {output}")
+
+
+def read_file(filename: Path, /, *, index: str = "") -> Optional[pd.DataFrame]:
+    """Read a file and return a DataFrame.
+
+    Parameters
+    ----------
+    filename : Path
+        File to open
+    index : str
+        Index name
+
+    Returns
+    -------
+    pd.DataFrame
+        Parsed data
+    """
+    try:
+        df = (
+            pd.read_csv(filename, header=0, index_col=0)
+            if filename.suffix == ".csv"
+            else pd.DataFrame(np.load(filename))
+        )
+        df.index.name = index
+    except Exception:
+        df = None
+    return df
