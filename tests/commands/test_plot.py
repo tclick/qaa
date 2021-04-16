842f722d64d6cd6b3329f5d3b0521a3d0a0c4122
Timothy Click <tclick@alumni.ou.edu>
2021-04-16 14:43:34 -0500

    test: run plot subcommand with CSV data
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

--- tests/commands/test_plot.py
+++ tests/commands/test_plot.py
@@ -13,3 +13,110 @@
 #  THIS SOFTWARE.
 # --------------------------------------------------------------------------------------
 """Test plot subcommand."""
+import logging
+import sys
+from pathlib import Path
+
+from pytest_console_scripts import ScriptRunner
+
+from ..datafile import PROJ
+from ..datafile import PROJNP
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+logger = logging.getLogger(name="ambgen.commands.cmd_qaa")
+
+if not sys.warnoptions:
+    import os
+    import warnings
+
+    warnings.simplefilter("default")  # Change the filter in this process
+    os.environ["PYTHONWARNINGS"] = "default"  # Also affect subprocesses
+
+
+class TestPlot:
+    """Run test for cluster subcommand."""
+
+    def test_help(self, script_runner: ScriptRunner) -> None:
+        """Test help output.
+
+        GIVEN the cluster subcommand
+        WHEN the help option is invoked
+        THEN the help output should be displayed
+
+        Parameters
+        ----------
+        script_runner : ScriptRunner
+            Command-line runner
+        """
+        result = script_runner.run(
+            "qaa",
+            "plot",
+            "-h",
+        )
+        assert result.success
+
+    def test_plot_csv(self, script_runner: ScriptRunner, tmp_path: Path) -> None:
+        """Test plot subcommand with CSV input file.
+
+        GIVEN a data file
+        WHEN invoking the plot subcommand
+        THEN saves a plotted image to disk
+
+        Parameters
+        ----------
+        script_runner : ScriptRunner
+            Command-line runner
+        tmp_path : Path
+            Temporary directory
+        """
+        outfile = tmp_path.joinpath("projection.png")
+        logfile = tmp_path.joinpath("plot.log")
+        result = script_runner.run(
+            "qaa",
+            "plot",
+            "-i",
+            PROJ,
+            "-o",
+            outfile.as_posix(),
+            "-l",
+            logfile.as_posix(),
+            "--pca",
+            "--verbose",
+        )
+        assert result.success
+        assert logfile.exists()
+        assert outfile.exists()
+        assert outfile.stat().st_size > 0
+
+    def test_plot_npy(self, script_runner: ScriptRunner, tmp_path: Path) -> None:
+        """Test plot subcommand with NumPy input file.
+
+        GIVEN a data file
+        WHEN invoking the plot subcommand
+        THEN saves a plotted image to disk
+
+        Parameters
+        ----------
+        script_runner : ScriptRunner
+            Command-line runner
+        tmp_path : Path
+            Temporary directory
+        """
+        outfile = tmp_path.joinpath("projection.png")
+        logfile = tmp_path.joinpath("plot.log")
+        result = script_runner.run(
+            "qaa",
+            "plot",
+            "-i",
+            PROJNP,
+            "-o",
+            outfile.as_posix(),
+            "-l",
+            logfile.as_posix(),
+            "--pca",
+            "--verbose",
+        )
+        assert result.success
+        assert logfile.exists()
+        assert outfile.exists()
+        assert outfile.stat().st_size > 0
